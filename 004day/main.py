185e5ab4e85fa0b79f959a77da298234c175c0f8
Billy Bones <xrayfist@gmail.com>
2024-02-27 09:50:28 +0200

    day004

---
+++ 004day/main.py
@@ -0,0 +1,13 @@
+import random
+import my_module
+
+random_integer = random.randint(0, 4)
+print(random_integer)
+
+random_float = random.random()
+print(random_float)
+
+print(random_integer + random_float)
+print(random_float * 5)
+
+print(my_module.pi)
