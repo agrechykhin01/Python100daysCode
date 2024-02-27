7c6dd7560929e1e83203e9a9510370763cb65c5c
Billy Bones <xrayfist@gmail.com>
2024-02-27 20:55:13 +0200

    day004

--- 004day/main.py
+++ 004day/main.py
@@ -1,13 +1,17 @@
-import random
-import my_module
+# import random
+# import my_module
 
-random_integer = random.randint(0, 4)
-print(random_integer)
+# random_integer = random.randint(0, 4)
+# print(random_integer)
 
-random_float = random.random()
-print(random_float)
+# random_float = random.random()
+# print(random_float)
 
-print(random_integer + random_float)
-print(random_float * 5)
+# print(random_integer + random_float)
+# print(random_float * 5)
 
-print(my_module.pi)
+# print(my_module.pi)
+
+states_of_america = ["Delaware", "Pennsylvania"]
+
+print(states_of_america[0])
