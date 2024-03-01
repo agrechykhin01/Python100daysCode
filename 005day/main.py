b4b6ef08b265d4de92969a94231f2d8e50281a4e
Billy Bones <xrayfist@gmail.com>
2024-03-01 09:29:16 +0200

    005day

--- 005day/main.py
+++ 005day/main.py
@@ -1,4 +1,10 @@
-fruits = ["Apple", "Peach", "Pear"]
+# fruits = ["Apple", "Peach", "Pear"]
 
-for fruit in fruits:
-    print(fruit)
+# for fruit in fruits:
+#     print(fruit)
+
+sum = 0
+
+for number in range(1, 101):
+    sum += number    
+print(sum)
