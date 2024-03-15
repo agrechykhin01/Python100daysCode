4199141f2c64b4b92c7c41de032ec38f4bbd7096
Billy Bones <xrayfist@gmail.com>
2024-03-15 09:44:44 +0200

    008

---
+++ 008day/main.py
@@ -0,0 +1,17 @@
+# def greet():
+#     print("Hello!")
+#     print("How do you do?")
+#     print("Isn't the weather nice today?")
+
+
+# def greet_with_name(name):
+#     print(f"Hello, {name}!")
+#     print(f"How do you do, {name}?")
+#     print(f"Isn't the weather nice today, {name}?")
+
+def greet_with(name, location):
+    print(f"Hello, {name}!")
+    print(f"How do you do, {name}?")
+    print(f"Isn't the weather nice today in {location}?")
+
+greet_with(location = "London", name = "Billy")
