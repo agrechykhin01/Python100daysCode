66832f12439511888c2983d1f9c2fff00ac5e3ec
Billy Bones <xrayfist@gmail.com>
2024-03-18 19:54:29 +0200

    008

--- 008day/caesar-cypher.py
+++ 008day/caesar-cypher.py
@@ -1,31 +1,23 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(text, shift):
-    encrypted = ""
+def caesar(text, shift, direction):
+    result_text = ""
     for letter in text:
         letter_index = alphabet.index(letter)
-        new_letter_index = letter_index + shift
-        if new_letter_index > (len(alphabet) - 1):
-            new_letter_index = new_letter_index % len(alphabet)
-        encrypted += alphabet[new_letter_index]
-    print(f"The encoded text is: {encrypted}")
 
-def decrypt(text, shift):
-    decrypted = ""
-    for letter in text:
-        letter_index = alphabet.index(letter)
-        new_letter_index = letter_index - shift
-        if new_letter_index < 0:
-            new_letter_index = len(alphabet) - ((-new_letter_index) % (len(alphabet) - 1))
-        decrypted += alphabet[new_letter_index]
-    print(f"The decoded text is: {decrypted}")
+        if direction == "encode":
+            new_letter_index = letter_index + shift
+            result_text += alphabet[new_letter_index]
+        elif direction == "decode":
+            new_letter_index = letter_index - shift
+            result_text += alphabet[new_letter_index]
+        else:
+            print("Type only 'encode' or 'decode' as direction!")
 
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
+    print(f"The {direction}d text is: {result_text}")
+
+caesar(text, shift, direction)
