df249fa80edf37713166113c102408bde0c55b67
Billy Bones <xrayfist@gmail.com>
2024-03-20 19:15:47 +0200

    009

---
+++ 009day/main.py
@@ -0,0 +1,12 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing something over and over again."
+}
+
+# print(programming_dictionary["Bug"])
+
+programming_dictionary["Comment"] = "Comment for your code"
+
+for item in programming_dictionary:
+    print(programming_dictionary[item])
